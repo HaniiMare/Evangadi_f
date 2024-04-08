838bfcd7123dc4f99a54b857dc737d10f3e3c0a5
HaniiMare <152915450+HaniiMare@users.noreply.github.com>
2024-04-08 15:17:13 -0700

    Change server from local to remote

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -2,10 +2,9 @@ import axios from 'axios'
 
 const axiosBase = axios.create({
   //Local server
-  baseURL: 'http://localhost:5500/api',
+  // baseURL: 'http://localhost:5500/api',
 
   //Remote server on Render.com
-  // baseURL: "https://eva-forum.onrender.com/api"
-
-})
+  baseURL: "https://evangadi-f.onrender.com/api",
+});
 export default axiosBase;
